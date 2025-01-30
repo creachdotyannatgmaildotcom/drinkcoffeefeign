2508253194898f7f0658fb1881b76614c13416db
fletcher <bigpanel@gmail.com>
2025-01-30 15:13:09 +0100

    coffeeclient

--- src/main/java/org/formation/client/CoffeeClient.java
+++ src/main/java/org/formation/client/CoffeeClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "drink")
+@FeignClient(value = "COFFEEMACHINE")
 public interface CoffeeClient {
 	@GetMapping("coffees/{id}")
 	ResponseEntity<CoffeeDto> getCoffeeById(@PathVariable Long id);
